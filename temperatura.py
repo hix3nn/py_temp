12b84a57fc936bc93590ba6db053d6241d413245
Guilherme Mendes <guilhermemendes789@gmail.com>
2018-08-10 17:36:18 +0000

    Fixes print if user is outside from br

--- temperatura.py
+++ temperatura.py
@@ -1,26 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 import os
-
+type_temp = ['Hi:','Lo: ']
 
 def Main():
     city = input('Type your city: ')
     country = input('If your country is not Brazil type,else press enter: ')
     if country is None:
-        country ='Brazil'
-    source = requests.get('https://www.foreca.com/'+country+'/'+city.replace(' ','_')+'?tenday').text
+        country = 'Brazil'
+    source = requests.get('https://www.foreca.com/'+country+'/'+city.replace(' ','_')+'?tenday&quick_units=metric').text
     soup = BeautifulSoup(source, "html5lib")
-    type_temp =['Hi: ','Lo: ']
     os.system('cls')
     for each_forecast in soup.find_all('a',class_='cell'):
         print('\n'+'=== '+ each_forecast.find('span',class_='h5').text + ' ===')
-        print(each_forecast.div['title'])
+        print(' '+each_forecast.div['title'])
         count = 0
         for each_temp in each_forecast.find_all('strong'):
-            if '+' in each_temp.text:
+            if '°' in each_temp.text:
                 print('- '+type_temp[count]+each_temp.text.replace('+','')+'C')
             else:
-                print('- '+each_temp.text + ' Km/h')
+                print('- Winds: '+each_temp.text + ' Km/h')
             count += 1
     os.system('pause')
 Main()
